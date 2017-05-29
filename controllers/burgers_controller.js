cb6133c17ba50b387c41872bb87272285ee93e7b
Joe Kozlowski <kozineffect@github.com>
2017-05-29 15:57:32 -0400

    Created and finished basic functionality of website.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burgers = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burgers.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burgers.insertOne([
+    "burger_name", "devour"
+  ], [
+    req.body.name, req.body.devour
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burgers.updateOne({
+    devour: true
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devour) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devour) VALUES ('Double Whopper', false);
+INSERT INTO burgers (burger_name, devour) VALUES ('Baconater', false);
